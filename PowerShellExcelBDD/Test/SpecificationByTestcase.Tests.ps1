f221a0cd1ea7b4e70cb3f3df816c64e0c6cedf04
Mike Zhang <zhangkq2000@hotmail.com>
2021-05-30 13:15:11 +0800

    pass get excel file of testcase

--- PowerShellExcelBDD/Test/SpecificationByTestcase.Tests.ps1
+++ PowerShellExcelBDD/Test/SpecificationByTestcase.Tests.ps1
@@ -11,7 +11,20 @@ Describe "Get Speicification by Example & Testcase" {
         -HeaderRow 4
 
     It "SpecificationByTestcase" -Testcases $SpecificationByTestcaseList {
-        $Header | Should -Be ""
+        $Error.Clear()
+        $Header | Should -Be $HeaderRowExpected
+        $Error.Count | Should -Be 0
+        ($HeaderRowTestResult -eq 'pass') | Should -Be $true
+
+        $TestExcelPath = "$StartPath\..\JavaExcelBDD\src\test\resources\$ExcelFileName"
+        $TestcaseList = Get-MZExampleWithTestResultList -ExcelPath $TestExcelPath `
+            -WorksheetName  $SheetName `
+            -ParameterNameColumn $ParameterNameColumn `
+            -HeaderRow $HeaderRow
+        
+        [bool]$TestcaseList | Should -Be ($ExcelFileNameExpected -eq 'got')
+        [bool]$TestcaseList | Should -Be ($SheetNameExpected -eq 'got')
+        
     }
     
 }
