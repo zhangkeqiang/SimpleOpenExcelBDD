8a8244027abd7aea5c226c88913c610349c66a2f
Mike Zhang <zhangkq2000@hotmail.com>
2021-06-12 08:22:03 +0800

    ImportExcel

--- PowerShellExcelBDD/CI/InitializeCI.ps1
+++ PowerShellExcelBDD/CI/InitializeCI.ps1
@@ -3,3 +3,6 @@ $global:StartPath = (Resolve-Path "$PSScriptRoot/..").Path
 Get-Module ExcelBDD | Remove-Module
 $modulePath = Join-Path $StartPath "ExcelBDD/ExcelBDD.psd1"
 Import-Module $modulePath
+if (-Not (Get-InstalledModule -Name ImportExcel)) {
+    Install-Module -Name ImportExcel -Scope CurrentUser
+}
