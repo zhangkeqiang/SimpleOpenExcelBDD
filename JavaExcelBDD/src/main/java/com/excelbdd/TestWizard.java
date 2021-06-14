1eb4ca0d6663bdcdc8d179ee74ad25ab847c01c8
Mike Zhang <zhangkq2000@hotmail.com>
2021-06-14 09:25:32 +0800

    fix SonarLint issue in TestWizard

--- JavaExcelBDD/src/main/java/com/excelbdd/TestWizard.java
+++ JavaExcelBDD/src/main/java/com/excelbdd/TestWizard.java
@@ -1,10 +1,13 @@
 package com.excelbdd;
+
 import java.io.File;
 
 public class TestWizard {
-    public static String getExcelBDDStartPath(String childPath) {
-        String absolutePath = new File(".").getAbsolutePath();
-        String startPath = absolutePath.substring(0, absolutePath.lastIndexOf(childPath));
-        return startPath;
-    }
+	private TestWizard() {
+	}
+
+	public static String getExcelBDDStartPath(String childPath) {
+		String absolutePath = new File(".").getAbsolutePath();
+		return absolutePath.substring(0, absolutePath.lastIndexOf(childPath));
+	}
 }
