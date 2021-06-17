27b197d78ebdfa32ecd955fe7fdf706bf8062859
Mike Zhang <zhangkq2000@hotmail.com>
2021-06-18 07:11:59 +0800

    testGetExampleListWithExpectedByMatcher

--- JavaExcelBDD/src/test/java/com/excelbdd/ExcelBDDSBTTest.java
+++ JavaExcelBDD/src/test/java/com/excelbdd/ExcelBDDSBTTest.java
@@ -19,13 +19,41 @@ class ExcelBDDSBTTest {
 		List<Map<String, String>> list = Behavior.getExampleListWithExpected(filepath, "Expected1", 2, 'B');
 		return list.stream();
 	}
-
 	/**
 	 * Test method for {@link com.excelbdd.Behavior#getExampleListWithExpected(java.lang.String, java.lang.String, int, char)}.
 	 */
 	@ParameterizedTest(name = "#{index}-TestExpected : {0}")
 	@MethodSource("provideExampleListWithExpected")
 	void testGetExampleListWithExpected(Map<String, String> mapParams) {
+		assertNotNull(mapParams);
+		TestWizard.showMap(mapParams);
+		assertTrue(mapParams.get("ParamName1").startsWith("V1."));
+		assertTrue(mapParams.get("ParamName2").startsWith("V2."));
+		assertEquals(true,mapParams.get("ParamName3").isEmpty());
+		assertEquals(false,mapParams.get("ParamName4").isEmpty());
+		
+		assertTrue(mapParams.get("ParamName1Expected").startsWith("V1."));
+		assertTrue(mapParams.get("ParamName2Expected").startsWith("V2."));
+		assertEquals(true,mapParams.get("ParamName3Expected").isEmpty());
+		assertEquals(false,mapParams.get("ParamName4Expected").isEmpty());
+		
+		assertNull(mapParams.get("ParamName1TestResult"));
+		assertNull(mapParams.get("ParamName2TestResult"));
+		assertNull(mapParams.get("ParamName3TestResult"));
+		assertNull(mapParams.get("ParamName4TestResult"));
+	}
+
+	static Stream<Map<String, String>> provideExampleListWithExpectedByMatcher() {
+		String filepath = TestWizard.getExcelBDDStartPath("JavaExcelBDD") + "BDDExcel/ExcelBDD.xlsx";
+		List<Map<String, String>> list = Behavior.getExampleListWithExpected(filepath, "Expected1", 2, 'B',"Scenario");
+		return list.stream();
+	}
+	/**
+	 * Test method for {@link com.excelbdd.Behavior#getExampleListWithExpected(java.lang.String, java.lang.String, int, char, java.lang.String)}.
+	 */
+	@ParameterizedTest(name = "#{index}-TestExpected : {0}")
+	@MethodSource("provideExampleListWithExpectedByMatcher")
+	void testGetExampleListWithExpectedByMatcher(Map<String, String> mapParams) {
 		assertNotNull(mapParams);
 		TestWizard.showMap(mapParams);
 		assertTrue(mapParams.get("ParamName1").startsWith("V1."));
