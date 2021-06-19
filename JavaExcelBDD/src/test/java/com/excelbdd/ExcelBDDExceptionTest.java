b9d82da49d51afa09f2729cccc91c11b27cf370d
Mike Zhang <zhangkq2000@hotmail.com>
2021-06-20 00:39:26 +0800

    judge exception

--- JavaExcelBDD/src/test/java/com/excelbdd/ExcelBDDExceptionTest.java
+++ JavaExcelBDD/src/test/java/com/excelbdd/ExcelBDDExceptionTest.java
@@ -2,18 +2,13 @@ package com.excelbdd;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ExcelBDDExceptionTest {
 
@@ -26,21 +21,18 @@ class ExcelBDDExceptionTest {
 	@ParameterizedTest(name = "#{index}-TestException: {0}")
 	@MethodSource("provideWrongExampleList")
 	void testGetExampleListStringString(Map<String, String> mapParams) {
-		String filepath = TestWizard.getExcelBDDStartPath("JavaExcelBDD") + "BDDExcel/" + mapParams.get("ExcelFileName");
+		String filepath = TestWizard.getExcelBDDStartPath("JavaExcelBDD") + "BDDExcel/"
+				+ mapParams.get("ExcelFileName");
 		int headerRow = Behavior.getInt(mapParams.get("HeaderRow"));
 		char parameterNameColumn = mapParams.get("ParameterNameColumn").charAt(0);
-		List<Map<String, String>> targetlist;
-		try {
-			targetlist = Behavior.getExampleListWithExpected(filepath,
+		Throwable exception = assertThrows(IOException.class, () -> {
+			List<Map<String, String>> targetlist = Behavior.getExampleListWithExpected(filepath,
 					mapParams.get("SheetName"), headerRow, parameterNameColumn);
-			assertNull(targetlist);
-		} catch (Exception e) {
-			System.out.println(e.toString());
-			System.out.println(e.getClass().getSimpleName());
-			System.out.println(e.getMessage());
-			assertEquals(mapParams.get("ExcelFileNameExpected"), e.getClass().getSimpleName());
-			assertTrue(e.getMessage().indexOf(mapParams.get("SheetNameExpected")) >= 0);
-		}
+		});
 		
+		System.out.println(exception.toString());
+		System.out.println(exception.getClass().getSimpleName());
+		assertEquals(mapParams.get("ExcelFileNameExpected"), exception.getClass().getSimpleName());
+		assertTrue(exception.getMessage().indexOf(mapParams.get("SheetNameExpected")) >= 0);
 	}
 }
