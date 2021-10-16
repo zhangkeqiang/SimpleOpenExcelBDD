9e577a9d7e0d29c40f781a60643e16dfa81ecbdc
ZhangMike <zhangkq2000@hotmail.com>
2021-10-16 22:44:17 +0800

    start DotNetExcelBDD

---
+++ DotNetExcelBDD/ExcelBDD/Behavior.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExcelBDD
+{
+    public static class Behavior
+    {
+         public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
