4fe5196755db309f50641f19b03adb5ec3237e24
ZhangMike <zhangkq2000@hotmail.com>
2021-11-09 11:57:55 +0800

    Include="DocumentFormat.OpenXml"

--- DotNetExcelBDD/ExcelBDD/Behavior.cs
+++ DotNetExcelBDD/ExcelBDD/Behavior.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-// using DocumentFormat.OpenXml;
-// using DocumentFormat.OpenXml.Packaging;
-// using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace ExcelBDD
 {
@@ -19,50 +19,47 @@ namespace ExcelBDD
 
         public static List<Dictionary<string, string>> GetExampleList(String filePath, String sheetName)
         {
-            // try
-            // {
-            //     //specify the file name where its actually exist   
-            //     string filepath = "D:\\TPMS\\Uploaded_Boq\\test.xlsx";
+            try
+            {
+                //open the excel using openxml sdk  
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, false))
+                {
 
-            //     //open the excel using openxml sdk  
-            //     using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filepath, false))
-            //     {
+                    //create the object for workbook part  
+                    WorkbookPart wbPart = doc.WorkbookPart;
 
-            //         //create the object for workbook part  
-            //         WorkbookPart wbPart = doc.WorkbookPart;
+                    //statement to get the count of the worksheet  
+                    int worksheetcount = doc.WorkbookPart.Workbook.Sheets.ChildElements.Count;
 
-            //         //statement to get the count of the worksheet  
-            //         int worksheetcount = doc.WorkbookPart.Workbook.Sheets.Count();
+                    //statement to get the sheet object  
+                    Sheet mysheet = (Sheet)doc.WorkbookPart.Workbook.Sheets.ChildElements.GetItem(0);
 
-            //         //statement to get the sheet object  
-            //         Sheet mysheet = (Sheet)doc.WorkbookPart.Workbook.Sheets.ChildElements.GetItem(0);
+                    //statement to get the worksheet object by using the sheet id  
+                    Worksheet Worksheet = ((WorksheetPart)wbPart.GetPartById(mysheet.Id)).Worksheet;
 
-            //         //statement to get the worksheet object by using the sheet id  
-            //         Worksheet Worksheet = ((WorksheetPart)wbPart.GetPartById(mysheet.Id)).Worksheet;
-
-            //         //Note: worksheet has 8 children and the first child[1] = sheetviewdimension,....child[4]=sheetdata  
-            //         int wkschildno = 4;
+                    //Note: worksheet has 8 children and the first child[1] = sheetviewdimension,....child[4]=sheetdata  
+                    int wkschildno = 4;
 
 
-            //         //statement to get the sheetdata which contains the rows and cell in table  
-            //         SheetData Rows = (SheetData)Worksheet.ChildElements.GetItem(wkschildno);
+                    //statement to get the sheetdata which contains the rows and cell in table  
+                    SheetData Rows = (SheetData)Worksheet.ChildElements.GetItem(wkschildno);
 
 
-            //         //getting the row as per the specified index of getitem method  
-            //         Row currentrow = (Row)Rows.ChildElements.GetItem(1);
+                    //getting the row as per the specified index of getitem method  
+                    Row currentrow = (Row)Rows.ChildElements.GetItem(1);
 
-            //         //getting the cell as per the specified index of getitem method  
-            //         Cell currentcell = (Cell)currentrow.ChildElements.GetItem(1);
+                    //getting the cell as per the specified index of getitem method  
+                    Cell currentcell = (Cell)currentrow.ChildElements.GetItem(1);
 
-            //         //statement to take the integer value  
-            //         string currentcellvalue = currentcell.InnerText;
+                    //statement to take the integer value  
+                    string currentcellvalue = currentcell.InnerText;
 
-            //     }
-            // }
-            // catch (Exception Ex)
-            // {
+                }
+            }
+            catch (Exception Ex)
+            {
 
-            // }
+            }
             List<Dictionary<string, string>> exampleList = new List<Dictionary<string, string>>();
             return exampleList;
         }
