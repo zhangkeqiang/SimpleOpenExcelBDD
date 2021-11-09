2b5f3def278fbe27720d15e1894e897aec830d83
ZhangMike <zhangkq2000@hotmail.com>
2021-11-10 00:12:57 +0800

    complete datatable

--- DotNetExcelBDD/ExcelBDD/Behavior.cs
+++ DotNetExcelBDD/ExcelBDD/Behavior.cs
@@ -9,11 +9,10 @@ namespace ExcelBDD
 {
     public static class Behavior
     {
-        public static IEnumerable<object[]> GetDataTable(String filePath, String sheetName, int headerRow)
+        public static IEnumerable<object[]> GetDataTableByArray(String filePath, String sheetName, int headerRow)
         {
             List<object[]> exampleList = new List<object[]>();
             List<string> Headers = new List<string>();
-            // DataTable dt = new DataTable();
             //open the excel using openxml sdk  
             using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, false))
             {
@@ -52,7 +51,7 @@ namespace ExcelBDD
                     {
                         // dt.Rows.Add();
                         // Dictionary<string, string> dic = new System.Collections.Generic.Dictionary<string, string>();
-                        object[] values =new object[columnCount];
+                        object[] values = new object[columnCount];
                         int i = 0;
                         Console.Write(counter);
                         Console.Write(": ");
@@ -70,26 +69,96 @@ namespace ExcelBDD
                 }
 
             }
-
             return (exampleList as IEnumerable<object[]>);
         }
 
+        public static IEnumerable<object[]> GetDataTable(String filePath, String sheetName, int headerRow)
+        {
+            List<object[]> exampleList = new List<object[]>();
+            List<string> Headers = new List<string>();
+            // DataTable dt = new DataTable();
+            //open the excel using openxml sdk  
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(filePath, false))
+            {
+                Sheets sheets = doc.WorkbookPart.Workbook.Sheets;
+                String sheetIdValue = null;
+                foreach (Sheet eachsheet in sheets)
+                {
+                    Console.WriteLine(eachsheet.Name);
+                    if (eachsheet.Name == sheetName)
+                    {
+                        sheetIdValue = eachsheet.Id.Value;
+                        break;
+                    }
+                }
+
+                Worksheet worksheet = (doc.WorkbookPart.GetPartById(sheetIdValue) as WorksheetPart).Worksheet;
+                IEnumerable<Row> rows = worksheet.GetFirstChild<SheetData>().Descendants<Row>();
+                int counter = 0;
+                int columnCount = 0;
+                foreach (Row row in rows)
+                {
+                    counter = counter + 1;
+                    //Read the headerRow row as header
+                    if (counter == headerRow)
+                    {
+                        foreach (Cell cell in row.Descendants<Cell>())
+                        {
+                            var colunmName = GetCellValue(doc, cell);
+                            columnCount++;
+                            Console.WriteLine(colunmName);
+                            Headers.Add(colunmName);
+                        }
+                    }
+                    else if (counter > headerRow)
+                    {
+                        Dictionary<string, string> dic = new System.Collections.Generic.Dictionary<string, string>();
+                        int i = 0;
+                        Console.Write(counter);
+                        Console.Write(": ");
+                        foreach (Cell cell in row.Descendants<Cell>())
+                        {
+                            String cellValue = GetCellValue(doc, cell);
+                            Console.Write(cellValue + " | ");
+                            dic.Add(Headers[i], cellValue);
+                            i++;
+                        }
+                        Console.WriteLine();
+                        exampleList.Add(new object[] { dic });
+                    }
+                }
+            }
+
+            return (exampleList as IEnumerable<object[]>);
+        }
         private static string GetCellValue(SpreadsheetDocument doc, Cell cell)
         {
-            string value = "";
             try
             {
-                value = cell.CellValue.InnerText;
+                string value = cell.CellValue.InnerText;
                 if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
                 {
                     return doc.WorkbookPart.SharedStringTablePart.SharedStringTable.ChildElements.GetItem(int.Parse(value)).InnerText;
                 }
+                return value;
             }
             catch (NullReferenceException)
             {
                 // Console.WriteLine(cell.ToString() + " is null");
+                return "";
             }
-            return value;
+        }
+
+        public static String GetValue(Dictionary<string, string> dic, string parameterName)
+        {
+            try
+            {
+                return dic[parameterName];
+            }
+            catch (KeyNotFoundException e)
+            {
+                return "";
+            }
         }
     }
 }
--- DotNetExcelBDD/ExcelBDDTest/BehaviorTest.cs
+++ DotNetExcelBDD/ExcelBDDTest/BehaviorTest.cs
@@ -21,6 +21,26 @@ namespace ExcelBDDTest
             int count = 0;
             foreach (var item in exampleList)
             {
+                Dictionary<string, string> dic = (Dictionary<string, string>)item[0];
+                Console.Write(dic.ToString());
+                Console.WriteLine(dic["Header01"]);
+                count++;
+            }
+            Assert.AreEqual(7, count);
+        }
+
+        [TestMethod]
+        public void TestGetDataTableByArray()
+        {
+            String currentPath = Directory.GetCurrentDirectory();
+            System.Console.WriteLine(currentPath);
+            String filePath = currentPath.Substring(0, currentPath.IndexOf("DotNetExcelBDD")) + "BDDExcel\\DataTableBDD.xlsx";
+            System.Console.WriteLine(filePath);
+            IEnumerable<object[]> exampleList = ExcelBDD.Behavior.GetDataTableByArray(filePath, "DataTable1", 2);
+            Assert.IsNotNull(exampleList);
+            int count = 0;
+            foreach (var item in exampleList)
+            {
                 count++;
             }
             Assert.AreEqual(7, count);
@@ -28,13 +48,35 @@ namespace ExcelBDDTest
 
 
         [DataTestMethod]
-        [DynamicData(nameof(GetData), DynamicDataSourceType.Method)]
-        public void TestDataTable1(String value1, String value2, String value3, String value4, String value5, String value6, String value7, String value8)
+        [DynamicData(nameof(GetDataByObjectArray), DynamicDataSourceType.Method)]
+        public void TestDataTableByObjectArray(String value1, String value2, String value3, String value4, String value5, String value6, String value7, String value8)
         {
-            Console.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}", value1,  value2,  value3,  value4,  value5,  value6,  value7,  value8);
+            Console.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}", value1, value2, value3, value4, value5, value6, value7, value8);
         }
 
-        public static IEnumerable<object[]> GetData()
+        public static IEnumerable<object[]> GetDataByObjectArray()
+        {
+            String currentPath = Directory.GetCurrentDirectory();
+            String filePath = currentPath.Substring(0, currentPath.IndexOf("DotNetExcelBDD")) + "BDDExcel\\DataTableBDD.xlsx";
+            Console.WriteLine(filePath);
+            return ExcelBDD.Behavior.GetDataTableByArray(filePath, "DataTable1", 2);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetDataByDictionary), DynamicDataSourceType.Method)]
+        public void TestDataTable(Dictionary<string, string> parameterDictionary)
+        {
+            Console.Write("{0}|", Behavior.GetValue(parameterDictionary, "Header01"));
+            Console.Write("{0}|", Behavior.GetValue(parameterDictionary, "Header02"));
+            Console.Write("{0}|", Behavior.GetValue(parameterDictionary, "Header03"));
+            Console.Write("{0}|", Behavior.GetValue(parameterDictionary, "Header04"));
+            Console.Write("{0}|", Behavior.GetValue(parameterDictionary, "Header05"));
+            Console.Write("{0}|", Behavior.GetValue(parameterDictionary, "Header06"));
+            Console.Write("{0}|", Behavior.GetValue(parameterDictionary, "Header07"));
+            Console.WriteLine("{0}", Behavior.GetValue(parameterDictionary, "Header08"));
+        }
+
+        public static IEnumerable<object[]> GetDataByDictionary()
         {
             String currentPath = Directory.GetCurrentDirectory();
             String filePath = currentPath.Substring(0, currentPath.IndexOf("DotNetExcelBDD")) + "BDDExcel\\DataTableBDD.xlsx";
